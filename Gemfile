44b3f45ffd17d0aba071e698319bc3b220f32f4d
Dylan Fareed <email@dylanfareed.com>
2014-01-20 09:27:53 -0500

    readme updates; adding middleman s3_sync for web accessible style guide

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'whitespace'
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-minify-html"
+gem 'middleman-s3_sync'
 
 gem 'haml'
 gem 'bootstrap-sass', '~> 3.0.3.0'
--- config.rb
+++ config.rb
@@ -77,13 +77,15 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :minify_html
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
@@ -91,3 +93,19 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+# AWS_SECRET= AWS_ACCESS_KEY= middleman s3_sync
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'partner-engineering-style-guide' # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY']
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default. 
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+end
+
